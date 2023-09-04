53a8679a0baee708b39ffaed1a5cd9bcdabb1a91
Luke Steadman <ljsteadman@gmail.com>
2023-09-04 15:08:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
+throw new Error('Oh no, a bug!');
+
 http.createServer(function (req, res) {
-  throw new Error('Oh no, a bug!');
   res.write('Hello World! This is a new test for 0.2.0-rc2');
   res.end();
 }).listen(3000);
